1e0ebb3e431738767118f642bd8e38d733fcf909
tanfengkui <tanfengkui@hotmail.com>
2017-01-30 16:24:35 +0800

    change

--- src/send mail.py
+++ src/send mail.py
@@ -4,14 +4,14 @@ from email.mime.text import MIMEText
 
 #############
 
-mailto_list=["tanfengkui@bsb.com.cn"]
+mailto_list=["name@company.com.cn"]
 
 
 #####################
 #设置服务器，用户名、口令以及邮箱的后缀
 mail_host="smtp.126.com"
-mail_user="tanfengkui"
-mail_pass="tfk12376576"
+mail_user="user"
+mail_pass="password"
 mail_postfix="126.com"
 
 ######################
